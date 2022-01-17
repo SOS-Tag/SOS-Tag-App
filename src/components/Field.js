213d58e4bea765b8ed11648ce2d69334d47431a9
benjamin <benjamin27briere@orange.fr>
2022-01-17 23:45:22 +0100

    [SignIn & SignUp] Add Password field type

--- src/components/Field.js
+++ src/components/Field.js
@@ -4,14 +4,18 @@ const Field = (props) => {
 
   function handleChange(event) {
     props.onChange(event.target.value);
-    console.log(event.target.value);
   }
 
   return ( 
     <div>
       <label className="Field">
+
         {props.label}{(props.mandatory)?"*":""}
-        <input className="FieldInput"type={props.label} value={props.value} name={props.text} onChange={handleChange} />
+        {(props.type === "password")?
+          <input minLength="8" className="FieldInput" type="password" name={props.text} onChange={handleChange} required/> 
+        : <input className="FieldInput"type={props.label} value={props.value} name={props.text} onChange={handleChange}/>
+        }
+        
       </label>
     </div>
    );
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -6,6 +6,10 @@ const SignUpPage = () => {
 
   const [surName, setSurName] = useState("");
   const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
 
   return ( 
     <div className="SignUpPageContainer">
@@ -19,7 +23,11 @@ const SignUpPage = () => {
         <div className="SignUpPageForm">
           <form>
             <Field editing={true} type="text" value={name} onChange={setName} label="Nom" mandatory={true}/>
-            <Field editing={false} type="text" value={surName} onChange={setSurName} label="Prenom" mandatory={false}/>
+            <Field editing={true} type="text" value={surName} onChange={setSurName} label="Prenom" mandatory={false}/>
+            <Field editing={true} type="mail" value={mail} onChange={setMail} label="Adresse mail" mandatory={true}/>
+            <Field editing={true} type="tel" value={phone} onChange={setPhone} label="Numéro de téléphone" mandatory={true}/>
+            <Field editing={true} type="password" onChange={setPassword1} label="Nouveau mot de passe" mandatory={true}/>
+            <Field editing={true} type="password" onChange={setPassword2} label="Répéter mot de passe" mandatory={true}/>
             <input type="submit" value="Submit" />
           </form>
           
