a717d8e16a488b17df21ab68146b7a356629a74b
benjamin <benjamin27briere@orange.fr>
2022-01-18 20:02:51 +0100

    [Tailwind] Update Components with Tailwind

--- src/components/Field.js
+++ src/components/Field.js
@@ -10,11 +10,11 @@ const Field = (props) => {
 
   return ( 
     <div>
-      <label className="Field">
+      <label className="Field w-1/2 flex flex-col text-left text-neutral-400">
 
         {props.label}{(props.mandatory)?"*":""}
         {(props.type === "password") ?
-          <input minLength="8" className="FieldInput" type="password" name={props.text} onChange={handleChange} required/> 
+          <input minLength="8" className="FieldInput p-0 w-full border-0 border-b-[2px] border-b-solid border-b-neutral-400 bg-transparent text-neutral-400 transition delay-200 focus:outline-none focus:border-SosTagYellow" type="password" name={props.text} onChange={handleChange} required/> 
         : (props.type === "tel") ?
            <PhoneInput 
             labels={fr}
@@ -24,7 +24,7 @@ const Field = (props) => {
             value={props.value}
             onChange={props.onChange}
            />
-          : <input className="FieldInput"type={props.label} value={props.value} name={props.text} onChange={handleChange}/>
+          : <input className="FieldInput p-0 w-full border-0 border-b-[2px] border-b-solid border-b-neutral-400 bg-transparent text-neutral-400 transition delay-200 focus:outline-none focus:border-SosTagYellow"type={props.label} value={props.value} name={props.text} onChange={handleChange}/>
         }
         
       </label>
