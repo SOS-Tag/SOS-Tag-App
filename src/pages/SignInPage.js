e764dbf9cf8fa3b8b73ddcda3b2f19f7b1a32556
kvaloris <keziah.valoris@gmail.com>
2022-01-19 10:56:49 +0100

    [signinpage, textmessage, signinform], create files

---
+++ src/pages/SignInPage.js
@@ -0,0 +1,11 @@
+import SignInForm from "../components/SignInForm"
+
+const SignInPage = () => {
+    return ( 
+        <div className="sign-in-page">
+            <SignInForm />
+        </div>
+     );
+}
+ 
+export default SignInPage;
