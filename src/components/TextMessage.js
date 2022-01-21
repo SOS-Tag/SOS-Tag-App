272f087df20723af3d97b9a2f35d84a017a98a9e
kvaloris <keziah.valoris@gmail.com>
2022-01-21 16:46:32 +0100

    [TextMessage] Update function + css

--- src/components/TextMessage.js
+++ src/components/TextMessage.js
@@ -4,26 +4,36 @@ const TextMessage = (props) => {
     let css='';
     let message ='';
 
-   if(props.type == 'error'){
-        css = "TextMessage flex flex-row space-x-4 p-2 w-auto w-90 text-white bg-red-500";
+    if(props.message){
+        css = "bg-red-500";
         img= "./assets/icon_error_textmessage.png";
-        message = "Aucun QR code associé à ce code d’indentification !";
-    }
-    if(props.type== 'modif'){
-        css = "TextMessage flex flex-row space-x-4 p-2 w-auto w-90 text-white bg-red-300";
-        img= "./assets/icon_modif_textmessage.png";
-        message = "Les modifications ont été prises en compte et ont été enregistrées !"
+        message = props.message;
     }
 
-   if(props.type== 'oups'){
-        css = "TextMessage flex flex-row space-x-4 p-2 w-auto w-90 text-white bg-blue-900";
-        img= "./assets/icon_oups_textmessage.png";
-        message = "Ceci est un petit problème !"
-
+    else{
+        if(props.type == 'error'){
+            css = "bg-red-500";
+            img= "./assets/icon_error_textmessage.png";
+            message = "Aucun QR code associé à ce code d’indentification !";
+        }
+        if(props.type== 'modif'){
+            css = "bg-red-300";
+            img= "./assets/icon_modif_textmessage.png";
+            message = "Les modifications ont été prises en compte et ont été enregistrées !"
+        }
+    
+       if(props.type== 'oups'){
+            css = "bg-blue-900";
+            img= "./assets/icon_oups_textmessage.png";
+            message = "Ceci est un petit problème !"
+    
+        }
     }
+
+   
 
     return (
-        <div className={css} >
+        <div className={"TextMessage flex flex-row space-x-4 p-2 w-auto w-90 text-white "+ css} >
             <img src={img} />
             <span className="message">
                 {message}
