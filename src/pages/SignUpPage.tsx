8b64ea543550309a355ed3f9bbd0e719b33f605a
benjamin <benjamin27briere@orange.fr>
2022-03-19 13:35:18 +0100

    [SignIn] Add text after register

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -67,6 +67,7 @@ const SignUpPage = () => {
       condition = false;
     }
     if(condition){
+      console.log("formulaire valide");
       // We use the register mutation as we implement the registration of a Firebase user
       // on the backend.
       // When a user is created, we also create an account which shares the id and the
@@ -79,10 +80,12 @@ const SignUpPage = () => {
       const error = response?.data?.register?.error;
 
       if (success) {
+        console.log('inscription valide')
         setIsRegistered(true);
       }
 
       if (error) {
+        console.log(error)
         setError(error?.message as string)
       }
 
@@ -100,56 +103,64 @@ const SignUpPage = () => {
         <img className="w-4/5" alt="imgSignUp" src="./assets/ImageSignUp.png"/>
       </div>
       <div className="w-full tablet:w-1/2 SignUpPageContent ">
-        <h1 className="mobile:mt-[50px] mb-[50px] SignUpPageTitle" >
-          Créez votre compte SOS Tag
-        </h1>
-        <div className="SignUpPageForm">
-          <form onSubmit={checkform} >
-            <div className="SignUpPageRow w-full grid">
-              <div className="SignUpPageRow-item-a">
-                <Field editing={true} type="text" name={"firstname"} label="Nom" mandatory={true}/>
-              </div>
-              <div className="SignUpPageRow-item-b">
-                <Field editing={true} type="text" name={"lastname"} label="Prenom" mandatory={false}/>
-              </div>
-            </div>
-            <div className="SignUpPageRow w-full grid">
-              <div className="SignUpPageRow-item-a">
-                <Field editing={true} type="mail" name={"mail"} label="Adresse mail" mandatory={false}/> 
-                {errorMail && <TextMessage type={TextMessageType.error} message="Mail invalide"/>}
-              </div>
-              <div className="SignUpPageRow-item-b">
-                <Field editing={true} type="tel" name={"phone"} label="Numéro de téléphone" mandatory={false}/>
-              </div>
-            </div>
-            <div className="SignUpPageRow w-full grid">
-              <div className="SignUpPageRow-item-a">
-                <Field editing={true} type="password" name={"password1"} label="Nouveau mot de passe" mandatory={false}/>
-                {errorPwd && <TextMessage type={TextMessageType.error} message="Mots de passe différents"/>}
-              </div>
-              <div className="SignUpPageRow-item-b">
-                <Field editing={true} type="password" name={"password2"} label="Répéter mot de passe" mandatory={false}/>
-              </div>  
-            </div>
+        {(isRegistered)
+          ? <h1 className="mobile:mt-[50px] mb-[50px] SignUpPageTitle" >
+              Un mail vous a été envoyé pour valider votre compte
+            </h1>
+          : <>
+              <h1 className="mobile:mt-[50px] mb-[50px] SignUpPageTitle" >
+                Créez votre compte SOS Tag
+              </h1>
+              <div className="SignUpPageForm">
+                <form onSubmit={checkform} >
+                  <div className="SignUpPageRow w-full grid">
+                    <div className="SignUpPageRow-item-a">
+                      <Field editing={true} type="text" name={"firstname"} label="Nom" mandatory={true}/>
+                    </div>
+                    <div className="SignUpPageRow-item-b">
+                      <Field editing={true} type="text" name={"lastname"} label="Prenom" mandatory={false}/>
+                    </div>
+                  </div>
+                  <div className="SignUpPageRow w-full grid">
+                    <div className="SignUpPageRow-item-a">
+                      <Field editing={true} type="mail" name={"mail"} label="Adresse mail" mandatory={false}/> 
+                      {errorMail && <TextMessage type={TextMessageType.error} message="Mail invalide"/>}
+                    </div>
+                    <div className="SignUpPageRow-item-b">
+                      <Field editing={true} type="tel" name={"phone"} label="Numéro de téléphone" mandatory={false}/>
+                    </div>
+                  </div>
+                  <div className="SignUpPageRow w-full grid">
+                    <div className="SignUpPageRow-item-a">
+                      <Field editing={true} type="password" name={"password1"} label="Nouveau mot de passe" mandatory={false}/>
+                      {errorPwd && <TextMessage type={TextMessageType.error} message="Mots de passe différents"/>}
+                    </div>
+                    <div className="SignUpPageRow-item-b">
+                      <Field editing={true} type="password" name={"password2"} label="Répéter mot de passe" mandatory={false}/>
+                    </div>  
+                  </div>
 
-            <div className="SignUpPageCheckbox flex ">
-              <input required className="mt-[6px]" type="checkbox" name="consent" onClick={handleConsent}/>
-              <p className="ml-[10px] w-[80%] mb-[20px] shrink text-SosTagBlue">
-                J'accepte les conditions générales d'utilisation et les conditions générales de vente. Je consens au traitement de mes données conformément à la politique de confidentialité de SOS Tag.
-              </p>
-            </div>
-            <div className='mobile:hidden tablet:block'>
-              <Button box="fill" type="general" buttonText="Valider"/>    
-            </div>
-            <div className='tablet:hidden w-[80vw] ml-[10vw] mb-[20px]'>
-              <Button box="fill" type="general" fullSize={true} buttonText="Valider"/>    
-            </div>
-            
-          </form>
-        </div>
-        <div className="w-[100vw] flex mobile:pb-[50px]">
-          <AlternativText className="mt-[30px] text-SosTagBlue" text="Déjà inscrit ? " linkText="Se connecter" link="./sign-in"/>
-        </div>
+                  <div className="SignUpPageCheckbox flex ">
+                    <input required className="mt-[6px]" type="checkbox" name="consent" onClick={handleConsent}/>
+                    <p className="ml-[10px] w-[80%] mb-[20px] shrink text-SosTagBlue">
+                      J'accepte les conditions générales d'utilisation et les conditions générales de vente. Je consens au traitement de mes données conformément à la politique de confidentialité de SOS Tag.
+                    </p>
+                  </div>
+                  <div className='mobile:hidden tablet:block'>
+                    <Button box="fill" type="general" buttonText="Valider"/>    
+                  </div>
+                  <div className='tablet:hidden w-[80vw] ml-[10vw] mb-[20px]'>
+                    <Button box="fill" type="general" fullSize={true} buttonText="Valider"/>    
+                  </div>
+                  
+                </form>
+              </div>
+              <div className="w-[100vw] flex mobile:pb-[50px]">
+                <AlternativText className="mt-[30px] text-SosTagBlue" text="Déjà inscrit ? " linkText="Se connecter" link="./sign-in"/>
+              </div> 
+            </>
+        }
+        
       </div>
     </div>
 
